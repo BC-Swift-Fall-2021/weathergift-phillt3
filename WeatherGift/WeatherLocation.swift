a45a823ad482b9fb4aaeba93baa9f27f09b15008
phillt3 <tracypb@bc.edu>
2021-10-04 17:51:12 -0400

    First Week  7 push test

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Phillip  Tracy on 10/4/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var  longitude: Double
+}
